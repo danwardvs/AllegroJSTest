3f876478af3ecc58f23ea7c1e1e49ee780e6c7a2
danwardvs <danwardvs@adsgames.net>
2015-11-19 22:22:32 -0500

    first bit of awesome.exe porting

--- HelloWorld/game.js
+++ HelloWorld/game.js
@@ -1,22 +1,58 @@
 var x = 100,y = 100;
+var scene1;
+var bobby;
+
+var bobX=600;
+var bobY=600;
+var flip;
+var loop;
+var angle;
+var h=25;
+var w=25;
 
 function draw()
-{
-	textout(canvas,font,"Hello World!",x,y,24,makecol(0,0,0));
+{	
+	draw_sprite(canvas,scene1,0,0);
+	
+	
 }
 
 function update()
 {
+	 draw_sprite( buffer, scene1, 0, 0);
+     if(flip==0){draw_sprite(buffer,bobby, 600, 600); flip=1; loop=1;}
+	if(flip==1 && loop==0){draw_sprite_h_flip(buffer,bobby, 600, 600); flip=0;}
+	loop=0;
 	
+	angle=angle+5;                      
+	rotate_sprite(buffer, bobby, 400, 600, itofix(angle));
+	
+	stretch_sprite(buffer, bobby, 500, 600, w, h);
+	
+	stretch_sprite(buffer, bobby, 300, 600, w, 50);
+	
+	stretch_sprite(buffer, bobby, 200, 600, 50, h);
+	
+	w++;
+	h++;
+	
+	if(w==75){w=25;}
+	if(h==75){h=25;}	
+}
+
+function setup(){
+	scene1 = load_bmp("scene1.bmp");
+	buffer = load_bmp("bobby.bmp");
 }
 
 function main()
 {
 	enable_debug('debug');
-	allegro_init_all("game_canvas", 640, 480);
+	allegro_init_all("game_canvas", 900,680);
+	setup();
 	ready(function(){
 		loop(function(){
-			clear_to_color(canvas,makecol(255,255,255));
+			clear_to_color(canvas,makecol(0,0,0));
 			update();
 			draw();
 		},BPS_TO_TIMER(60));
